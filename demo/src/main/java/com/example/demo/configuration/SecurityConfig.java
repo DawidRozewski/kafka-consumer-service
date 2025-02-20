2d61c402e03ec2a27190ed22d282df72f0e01b42
DawidRozewski <dawid.rrozewski@gmail.com>
2025-02-20 20:04:41 +0100

    CON-005

--- demo/src/main/java/com/example/demo/configuration/SecurityConfig.java
+++ demo/src/main/java/com/example/demo/configuration/SecurityConfig.java
@@ -5,9 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -22,19 +19,8 @@ public class SecurityConfig {
                         .requestMatchers("/consumer/user").hasRole("USER")
                         .anyRequest().authenticated()
                 )
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
-                .csrf(AbstractHttpConfigurer::disable)
-                .anonymous(Customizer.withDefaults())
-                .httpBasic(Customizer.withDefaults());
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
 
         return httpSecurity.build();
     }
-
-    @Bean
-    public JwtDecoder jwtDecoder() {
-        return NimbusJwtDecoder
-                .withJwkSetUri("http://localhost:8085/realms/testrealm/protocol/openid-connect/certs")
-                .build();
-    }
-
 }
